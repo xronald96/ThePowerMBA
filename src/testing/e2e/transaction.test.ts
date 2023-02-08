c1e2422cdbec5a6767d53dc22388f955543ec7e8
ronald <ronald881996@gmail.com>
2023-02-08 21:27:22 +0100

    using beforeAll for login

--- src/testing/e2e/transaction.test.ts
+++ src/testing/e2e/transaction.test.ts
@@ -9,6 +9,7 @@ import express from 'express';
 describe('Transaction route', () => {
 	let user1: any;
 	let user2: any;
+	let logingResult: any;
 	const parentAPP = express();
 	parentAPP.use(function (req: any, _res: any, next: any) {
 		// lets stub session middleware
@@ -21,8 +22,10 @@ describe('Transaction route', () => {
 		next();
 	});
 	parentAPP.use(app);
+
+	
 	beforeEach(async () => {
-		jest.setTimeout(10000);
+		jest.setTimeout(100000);
 	});
 
 	beforeAll(async () => {
@@ -35,6 +38,9 @@ describe('Transaction route', () => {
 		});
 		await user1.save();
 		await user2.save();
+		logingResult = await request(parentAPP)
+			.put('/login')
+			.send({ name: user1.name, password: realPassword });
 	});
 
 	afterAll(async () => {
@@ -43,13 +49,10 @@ describe('Transaction route', () => {
 	});
 
 	it('crate new transaction', async () => {
-		const result = await request(parentAPP)
-			.put('/login')
-			.send({ name: user1.name, password: realPassword });
 		await request(parentAPP)
 			.post('/transaction')
 			.send({ from: user1.id, to: user2.id, amount: 20 })
-			.set({ 'x-access-token': result.body?.response?.token })
+			.set({ 'x-access-token': logingResult.body?.response?.token })
 			.expect(HTTP_STATUS.OK)
 			.expect('Content-Type', /application\/json/);
 	});
@@ -59,35 +62,26 @@ describe('Transaction route', () => {
 		{ from: user2, to: undefined, amount: 3, undefinedValue: 'to' },
 		{ from: user2, to: user2, amount: undefined, undefinedValue: 'amount' },
 	])('Error when ${undefinedValue} is not provided', async ({ from, to, amount }) => {
-		const result = await request(parentAPP)
-			.put('/login')
-			.send({ name: user1.name, password: realPassword });
 		await request(parentAPP)
 			.post('/transaction')
 			.send({ from, to, amount })
-			.set({ 'x-access-token': result.body?.response?.token })
+			.set({ 'x-access-token': logingResult.body?.response?.token })
 			.expect(HTTP_STATUS.BAD_REQUEST);
 	});
 
 	it('Error when from or to user is not found in the DB', async () => {
-		const result = await request(parentAPP)
-			.put('/login')
-			.send({ name: user1.name, password: realPassword });
 		await request(parentAPP)
 			.post('/transaction')
 			.send({ from: user1.id, to: '41224d776a326fb40f000001', amount: 10 })
-			.set({ 'x-access-token': result.body?.response?.token })
+			.set({ 'x-access-token': logingResult.body?.response?.token })
 			.expect(HTTP_STATUS.NOT_FOUND);
 	});
 
 	it('Error when amount is bigger than balance', async () => {
-		const result = await request(parentAPP)
-			.put('/login')
-			.send({ name: user1.name, password: realPassword });
 		await request(parentAPP)
 			.post('/transaction')
 			.send({ from: user1.id, to: user2.id, amount: 1000 })
-			.set({ 'x-access-token': result.body?.response?.token })
+			.set({ 'x-access-token': logingResult.body?.response?.token })
 			.expect(HTTP_STATUS.BAD_REQUEST);
 	});
 
